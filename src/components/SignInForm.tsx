9027fb6f8444d6b2f4c40c51de8970b24af4a811
Robiul Haque <robiulcoc420@gmail.com>
2024-09-29 02:25:15 +0600

    complete responsive design signIn component with tailwind css

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -15,12 +15,10 @@ const SignInForm = () => {
 
     return (
         <section className="relative">
-            <div className="flex flex-col items-center w-[50%] absolute top-[20%] left-[26%]">
-                <h1 className="text-2xl font-bold leading-10 bg-gradient-to-r from-fuchsia-600 to-indigo-600 bg-clip-text text-transparent">Welcome Back Again!</h1>
-            </div>
-            <div className="flex justify-center items-center gap-x-14 h-screen bg-slate-50">
-                <img src={Todo} alt="Todo Image" className="w-[30%]" />
-                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-6 w-3/12">
+            <h1 className="text-2xl font-bold bg-gradient-to-r from-fuchsia-600 to-indigo-600 bg-clip-text text-transparent absolute top-[8%] lg:top-[20%] left-[17.5%] lg:left-[42%]">Welcome Back Again!</h1>
+            <div className="flex justify-center items-center flex-col lg:flex-row gap-y-12 lg:gap-x-14 h-screen bg-slate-50">
+                <img src={Todo} alt="Todo Image" className="w-[80%] md:w-[53%] lg:w-[30%]" />
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-6 lg:w-[30%]">
                     <FormControl>
                         <InputLabel htmlFor="component-outlined">Email</InputLabel>
                         <OutlinedInput type="email" {...register("Email", { required: true })} id="component-outlined" label="Email" />
@@ -44,7 +42,7 @@ const SignInForm = () => {
                             label="Password"
                         />
                     </FormControl>
-                    <div className="flex justify-between">
+                    <div className="flex justify-between gap-x-12 md:gap-x-28 lg:gap-x-0.5">
                         <small>Don,t have an account <span className="underline decoration-slate-950 text-gray-500 hover:text-gray-900 ease-in duration-300"><a href="#">Sign Up</a></span> here</small>
                         <small className="underline decoration-slate-950 text-gray-500 hover:text-gray-900 ease-in duration-300"><a href="#">Forgot Password</a></small>
                     </div>
