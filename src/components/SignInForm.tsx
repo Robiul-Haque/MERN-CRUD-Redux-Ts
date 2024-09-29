617dd8203dcb01f7fd4dd48f1d5aa0ffbb71357f
Robiul Haque <robiulcoc420@gmail.com>
2024-09-30 00:07:34 +0600

    change sign in component heading

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -15,7 +15,7 @@ const SignInForm = () => {
 
     return (
         <section className="relative">
-            <h1 className="text-2xl font-bold bg-gradient-to-r from-fuchsia-600 to-indigo-600 bg-clip-text text-transparent absolute top-[8%] lg:top-[20%] left-[17.5%] lg:left-[42%]">Welcome Back Again!</h1>
+            <h1 className="text-2xl font-bold bg-gradient-to-r from-fuchsia-600 to-indigo-600 bg-clip-text text-transparent absolute top-[8%] lg:top-[20%] left-[17.5%] lg:left-[42%]">WELCOME BACK AGAIN!</h1>
             <div className="flex justify-center items-center flex-col lg:flex-row gap-y-12 lg:gap-x-14 h-screen bg-slate-50">
                 <img src={SignInTodo} alt="Todo Image" className="w-[80%] md:w-[53%] lg:w-[30%]" />
                 <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-6 lg:w-[30%]">
