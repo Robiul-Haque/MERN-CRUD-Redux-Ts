d1188966ba6f22899f3d37e93e96619781b4b846
Robiul Haque <robiulcoc420@gmail.com>
2024-10-04 00:18:09 +0600

    install redux persist & setup complete

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { Provider } from 'react-redux';
-import { store } from './redux/store.ts';
-import { RouterProvider } from 'react-router-dom';
-import router from './route/route.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.ts";
+import { RouterProvider } from "react-router-dom";
+import router from "./route/route.tsx";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor} />
       <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
