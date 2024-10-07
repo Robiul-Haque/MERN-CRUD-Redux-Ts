8692605a5c7f6312c1257605b10553251ae46124
Robiul Haque <robiulcoc420@gmail.com>
2024-10-08 01:11:48 +0600

    make logout function in todo dashboard

--- src/layout/todoDashboard.tsx
+++ src/layout/todoDashboard.tsx
@@ -1,9 +1,19 @@
+import { toast } from "sonner";
+import { logout } from "../redux/features/auth/authSlice";
+import { useAppDispatch } from "../redux/hook"
 
 const todoDashboard = () => {
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    toast.success('Logged out successfully');
+  }
+
   return (
-    <div>
-      Todo Dashboard
-    </div>
+    <>
+      <button onClick={handleLogout} className="bg-gradient-to-r from-fuchsia-600 to-indigo-600 text-white font-semibold px-5 py-3 rounded-md cursor-pointer hover:from-fuchsia-700 hover:to-indigo-700 ease-in duration-1000 m-5">Logout</button>
+    </>
   )
 }
 
