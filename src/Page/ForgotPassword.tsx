b1b7c36c26bcd67a9627f73a9f574ac9bb99296e
Robiul-Haque <robiulcoc420@gmail.com>
2025-01-10 03:03:53 +0600

    create forgot password component and set the api with redux tool kit

--- src/Page/ForgotPassword.tsx
+++ src/Page/ForgotPassword.tsx
@@ -4,13 +4,13 @@ import ForgotPasswordForm from "../components/ForgotPasswordForm";
 const ForgotPassword = () => {
     return (
         <section className="relative">
-            <h1 className="text-2xl font-bold bg-gradient-to-r from-fuchsia-600 to-indigo-600 bg-clip-text text-transparent absolute top-[8%] lg:top-[20%] left-[17.5%] lg:left-[42%]">FORGOT PASSWORD!</h1>
+            <h1 className="text-2xl font-bold bg-gradient-to-r from-fuchsia-600 to-indigo-600 bg-clip-text text-transparent absolute top-[8%] lg:top-[20%] left-[20.5%] lg:left-[42%]">FORGOT PASSWORD!</h1>
             <div className="flex justify-center items-center flex-col lg:flex-row gap-y-12 lg:gap-x-14 h-screen bg-slate-50">
-                <img src={forgotPassword} alt="Todo Image" className="w-[80%] md:w-[53%] lg:w-[30%]" />
+                <img src={forgotPassword} alt="Todo Image" className="w-[70%] md:w-[53%] lg:w-[30%]" />
                 <ForgotPasswordForm />
             </div>
         </section>
     )
 }
 
-export default ForgotPassword
+export default ForgotPassword;
--- src/components/ForgotPasswordForm.tsx
+++ src/components/ForgotPasswordForm.tsx
@@ -1,6 +1,8 @@
 import { FormControl, InputLabel, OutlinedInput } from "@mui/material"
 import { useForm } from "react-hook-form";
 import { useForgotPasswordMutation } from "../redux/features/auth/authApi";
+import TransferWithinAStationIcon from '@mui/icons-material/TransferWithinAStation';
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 
 const ForgotPasswordForm = () => {
     const { register, handleSubmit } = useForm();
@@ -12,13 +14,14 @@ const ForgotPasswordForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-6 lg:w-[30%]">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-6 w-[80%] lg:w-[30%]">
+            <span className="flex justify-center items-baseline gap-x-2"><TransferWithinAStationIcon /> <p className="text-center text-xl font-bold text-slate-400">Find your account...</p></span>
             <FormControl>
-                <InputLabel htmlFor="component-outlined">Email</InputLabel>
+                <InputLabel htmlFor="component-outlined">Your Email</InputLabel>
                 <OutlinedInput type="email" {...register("email", { required: true })} id="component-outlined" label="Email" />
             </FormControl>
-            <input type="submit" value={"Submit"} className="bg-gradient-to-r from-fuchsia-600 to-indigo-600 text-white font-semibold px-5 py-3 rounded-md cursor-pointer hover:from-fuchsia-700 hover:to-indigo-700 ease-in duration-1000" />
-        </form>
+            <span className="flex justify-center items-center gap-x-2 bg-gradient-to-r from-fuchsia-600 to-indigo-600 text-white font-semibold px-5 py-3 rounded-md cursor-pointer hover:from-fuchsia-700 hover:to-indigo-700 ease-in duration-1000"><PersonSearchIcon /> <input type="submit" value={"Search"} /></span>
+        </form >
     )
 }
 
